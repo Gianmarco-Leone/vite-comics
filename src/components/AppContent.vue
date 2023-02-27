64a2f5c8a8aa2704425d13aff182d849361d8e5e
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-02-27 15:59:27 +0100

    add main component

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -3,9 +3,31 @@ export default {};
 </script>
 
 <template>
-  <div class="title">
-    <h1>MAIN</h1>
-  </div>
+  <section>
+    <div class="container">
+      <div class="content">
+        <span> --> Content goes here &lt;-- </span>
+      </div>
+    </div>
+  </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../assets/scss/mixin.scss" as *;
+
+section {
+  @include flex-center("vertical");
+  min-height: 150px;
+  background-color: #222;
+
+  .content {
+    // debug
+    width: 1200px;
+    span {
+      color: #fff;
+      font-size: 1.5rem;
+      font-weight: bold;
+    }
+  }
+}
+</style>
